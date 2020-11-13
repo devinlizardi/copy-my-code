f4374507817727639fd49130c8b2509e89e524e3
Devin Lizardi <dlizardi@berkeley.edu>
2020-11-12 16:47:39 -0800

    Most functionality complete!

--- src/App.js
+++ src/App.js
@@ -7,24 +7,20 @@ function App(props) {
       title="Testing Write"
       >
       {
-        `class Main {
-        public static void main(String[] args) {
-          String hello = "world";
-          Lower brad = new Lower();
+        ` class Main {
+            public static void main(String[] args) {
+              System.out.println("Hello World!");
+              private testMe = new Lower(22);
+              private testYou = new Lower(23);
+            }
+          }
 
-          brad.riseUp();
-        }
-      }
-      
-      class Lower {
-        private int population = 1;
-        private String[] names = ["bilbo", "bobo"];
-
-        public void riseUp() {
-          population++;
-          names.push("bob");
-        }
-      }`
+          class Lower {
+            private int x;
+            public static void lower(x) {
+              this.x = x;
+            }
+          }`
       }
       </CopyCode>
   );
--- src/CopyCode.js
+++ src/CopyCode.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function CopyCode(props) {
+    const {title} = props;
+    const clearAll = () => {
+        localStorage.clear();
+    }
+
     return(
         <>
         <div className="container">
-            <h2 className="title">{props.title}</h2>
+            <h2 className="title">{title}</h2>
             {parseJava(props)}
+            <button onClick={clearAll}>clear saved text</button>
         </div>
         </>
     );
@@ -14,7 +20,8 @@ function CopyCode(props) {
 function parseJava(props) {
     const text = props.children.split("\n");    // Array
     const textElem = [];                        // Array of JSX elements
-
+    const indentLevels = [];                    // Array of indent levels for overlay
+    
     var indentLevel = 0;
     var k = 0;
     
@@ -29,6 +36,7 @@ function parseJava(props) {
                         {str}
                     </p>
                     )
+        indentLevels.push(indentLevel)
 
         if (str[str.length - 1] === "{") {
             indentLevel += 2;
@@ -39,26 +47,40 @@ function parseJava(props) {
 
     return(
         <div className="code-text-wrapper">
-            <LineOverlay numLines={k}/>
+            <LineOverlay numLines={k} indentLevels={indentLevels} text={text}/>
             {textElem}
         </div>
     );
 }
 
 function LineOverlay(props) {
-    const {numLines} = props;
+    const {numLines, indentLevels} = props;
     const spanElems = [];
 
     for (let i=0; i<numLines; i++) {
+        let stored = localStorage.getItem("dataStorage-span-" + i)
         spanElems.push(
-            <span className="overlay-line" key={i}/>
-        )
-    }
+            <span className="overlay-line" key={i} id={"span-" + i} 
+            contentEditable suppressContentEditableWarning={true}
+            style={{"left" : indentLevels[i] + "em"}}>
+                {stored ? stored : "\xa0"}
+            </span>
+        );
+    } 
+
+    useEffect(() => {
+        const editables = document.querySelectorAll("[contenteditable]");
+        editables.forEach(el => {
+            el.addEventListener("blur", () => {
+                localStorage.setItem("dataStorage-" + el.id, el.innerText);
+            });
+        })
+    });
 
     return(
-    <>
+    <div className="overlay-wrapper" contentEditable={false}>
         {spanElems}
-    </>)
+    </div>)
 }
 
 export default CopyCode;
